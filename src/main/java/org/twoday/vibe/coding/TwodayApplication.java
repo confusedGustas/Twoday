d0e0d9baf5aeaf76da48d19d6e3eecedc809a07c
confusedGustas <gustas.antanas2003@gmail.com>
2025-06-08 09:12:07 +0300

    project init

---
+++ src/main/java/org/twoday/vibe/coding/TwodayApplication.java
@@ -0,0 +1,13 @@
+package org.twoday.vibe.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwodayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/twoday/vibe/coding/TwodayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.twoday.vibe.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
